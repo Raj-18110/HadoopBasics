049cb06be324d4222ce3610706ef10012ea5bee9
Raj-18110 <19566773+Raj-18110@users.noreply.github.com>
2021-04-23 12:30:13 +0530

    Add files via upload

--- Kafkademo.py
+++ Kafkademo.py
@@ -1,6 +1,8 @@
 from pyspark.sql.session import SparkSession
 
 if __name__ == '__main__':
+    clusterPath = "/opt/spark/work-dir/job-1"
+    localPath = "output"
     data = [
         ["1201", "satish", "25"],
         ["1202", "krishna", "28"],
@@ -13,4 +15,4 @@ if __name__ == '__main__':
 
     deptDF = sparkSession.createDataFrame(data=data, schema=dataColumns)
     deptDF.printSchema()
-    deptDF.show(truncate=False)
+    deptDF.coalesce(1).write.option("header", True).csv(localPath)
